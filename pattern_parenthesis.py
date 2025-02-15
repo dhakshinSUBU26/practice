71bbe0fe2bce14e8f999185cce044cccf1f93ebb
Subramanian S <dhakshineaswar@gmail.com>
2025-02-15 23:21:09 +0530

    Update pattern_parenthesis.py

--- pattern_parenthesis.py
+++ pattern_parenthesis.py
@@ -1,4 +1,10 @@
-n=5
-for i in range(n):
-    for j in range(i+1):
-        print('{'*j,'}'*j,end=" ")
+#to print the pattern of parenthesis
+class para(object):
+    def parenthesis(self,n):
+        for i in range(n):
+            for j in range(i+1):
+                print('{'*j,'}'*j,end=" ")
+            print()
+
+obj=para()
+obj.parenthesis(5)
